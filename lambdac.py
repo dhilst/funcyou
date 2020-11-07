6f7f5b8ec7cbc181a535e1505270caeb9442f990
Daniel Hilst Selli <daniel@versatushpc.com.br>
2020-11-07 12:40:53 -0300

    Bind variables and better repr

--- lambdac.py
+++ lambdac.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import (
     Dict,
     Any,
@@ -37,25 +38,119 @@ from collections import namedtuple
 from functools import reduce
 from pprint import pprint
 
-T = TypeVar("T")
+
+T = TypeVar("T", bound="Term")
+V = TypeVar("V", bound="Variable")
 
 
-class Term:
+class Term(ABC):
+    """
+    Base class for Variable, Abstraction and Application
+    """
+
     def __repr__(self):
         args = ", ".join(f"{k}={repr(v)}" for k, v in self.__dict__.items())
         return f"{self.__class__.__name__}({args})"
 
+    @abstractmethod
+    def bind(self, other: "Variable") -> "Term":
+        "Should returns the new bound term"
+        pass
+
+
+class Variable(Term, ABC):
+    """
+    Base class for variables, an variable may be BoundVariable or FreeVariable
+    """
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def bind(self, other: "Variable") -> "Variable":
+        """
+        Return the bound variable if the names matches
+
+        The caller should assign the return value where appropriate
+        """
+        if self.name == other.name:
+            return other
+        else:
+            return self
+
+
+class FreeVariable(Variable):
+    """
+    Free variable, call bind_to to bind it
+    """
+
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    def bind_to(self, lambda_: "Lambda") -> "BoundVariable":
+        return BoundVariable(self.name, lambda_)
+
+    def __repr__(self):
+        return f"FV({self.name})"
+
+
+class BoundVariable(Variable):
+    """
+    Bound variable, use bound to get the lambda to where this variable is bounded
+    """
+
+    def __init__(self, name: str, lambda_: "Lambda"):
+        super().__init__(name)
+        self._bound = lambda_
+
+    @property
+    def bound(self):
+        return self._bound
+
+    def __repr__(self):
+        return f"BV({self.name})"
+
 
 class Lambda(Term):
-    def __init__(self, arg, body):
-        self.arg = arg
-        self.body = body
+    """
+    An abstraction, arg is the sole argument and body another Term
+    """
+
+    def __init__(self, arg, body: Term):
+        self.arg = arg.bind_to(self)
+        self.body = body.bind(self.arg)
+
+    def bind(self, var: Variable) -> "Lambda":
+        """
+        Bind an variable inside body, for example if
+        you have (fn a => fn b => a b), fn b => b
+        is constructed as Lambda(b, FV(a) BV(b)), then
+        fn a => .. calls bind on its argument and we get
+        Lambda(a, Lambda(b, BV(a) BV(b)))
+        """
+        self.body = self.body.bind(var)
+        return self
+
+    def __repr__(self):
+        return f"(λ{self.arg.name}.{self.body})"
 
 
 class Application(Term):
     def __init__(self, e1, e2):
         self.e1 = e1
         self.e2 = e2
+
+    def alpha_conversion(self, a, b):
+        return Application(
+            self.e1.alpha_conversion(a, b), self.e2.alpha_conversion(a, b)
+        )
+
+    def bind(self, var: Variable) -> "Application":
+        self.e1 = self.e1.bind(var)
+        self.e2 = self.e2.bind(var)
+        return self
+
+    def __repr__(self):
+        return f"{self.e1} {self.e2}"
 
 
 def to_lambda(t):
@@ -67,8 +162,14 @@ def to_application(t):
     return reduce(Application, t)
 
 
+def to_variable(t):
+    return FreeVariable(t[0])
+
+
 def BNF():
-    ID = Word(alphas, exact=1)
+    if hasattr(BNF, "cache"):
+        return BNF.cache
+    ID = Word(alphas, exact=1).setParseAction(to_variable)
     FN = Literal("fn").suppress()
     ARROW = Literal("=>").suppress()
     LP = Literal("(").suppress()
@@ -95,21 +196,30 @@ def BNF():
 
     term.validate()
 
+    BNF.cache = term
+
     return term
 
 
-BNF().runTests(
-    """
-    # Simple abstraction
-    fn a => a a b
+if __name__ == "__main__":
+    BNF().runTests(
+        """
+        # Simple abstraction
+        fn a => a a b
 
-    # Chainned abstraction
-    fn a => fn b => a b
+        # Chainned abstraction
+        fn a => fn b => a b
 
-    # Abstraction application
-    (fn a => a a) (fn b => b)
+        # Abstraction application
+        (fn a => a a) (fn b => b)
 
-    # Try left associativity of appliction
-    a b c d e
-    """
-)
+        # Try left associativity of appliction
+        a b c d e
+
+        # Simple application
+        (fn a => a) b
+
+        # ɑ conversion needed
+        (fn x => x y) y
+        """
+    )
